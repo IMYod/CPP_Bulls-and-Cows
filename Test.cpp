fd2f4056cb1a6d96291615a25dbd3a23069d99f2
Your Name <Nvicoder@example.com>
2019-04-29 14:27:53 -0400

    fixed the test

--- Test.cpp
+++ Test.cpp
@@ -18,23 +18,36 @@ using namespace std;
 
 using namespace bullpgia;
 
+/************function for our tests************/
+
+	string numToGuess(int num, uint length){
+       	string guess = to_string(num);
+       	int numOfZeros = length - guess.length();
+        for (int j=0; j<numOfZeros; ++j){
+		guess = "0" + guess;
+            }
+        return guess;
+        }
+/*****************************************/
 
 int main() {
 
 	badkan::TestCase testcase;
 	int grade=0;
 	int signal = setjmp(badkan::longjmp_buffer);
+	
+
 	if (signal == 0) {
 
 	// BASIC TESTS - DO NOT CHANGE
         ConstantChooser c1234{"1234"}, c12345{"12345"}, c9999{"9999"},c456789{"456789"};
-        ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"},g456789{"456789"},g11111{"111111"}g111121{"1111121"};
+        ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"},g456789{"456789"},g111111{"111111"},g111121{"1111121"};
 
 	testcase.setname("Calculate bull and pgia")
 	.CHECK_OUTPUT(calculateBullAndPgia("1234","1234"), "4,0")      // 4 bull, 0 pgia
 	.CHECK_OUTPUT(calculateBullAndPgia("1234","4321"), "0,4")      // 0 bull, 4 pgia
 	.CHECK_OUTPUT(calculateBullAndPgia("123","1234"), "3,0")      // 3 bull, 0 pgia         
-        
+        ;
         /*our test*/
         testcase.setname("Our tests")
 	.CHECK_OUTPUT(calculateBullAndPgia("4567","1254"), "0,1")      // 0 bull, 1 pgia            
@@ -65,7 +78,7 @@ int main() {
 	.CHECK_EQUAL(play(c12345, g1234, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
         
         /*our test*/
-
+	;
         testcase.setname("Our tests")
 	.CHECK_EQUAL(play(c456789, g456789, 6, 100), 1)      // guesser wins in one turn.
         .CHECK_EQUAL(play(c456789, g456789, 6, 100), 1)      // guesser wins in one turn.
@@ -101,16 +114,8 @@ int main() {
 	}
 		
 	
-      string numToGuess(int num, uint length){
-        string guess = to_string(num);
-        int numOfZeros = length - guess.length();
-        for (int i=0; i<numOfZeros; ++i){
-		guess = "0" + guess;
-            }
-        return guess;
-        }
-        
-	for (i=0,i<10000;i++){
+       
+	for (uint i=0,i<10000;i++){
          uint length=0;
         switch(length) {
           case (i<10) :
@@ -129,7 +134,7 @@ int main() {
            length =4;
          break;
    }
-   ConstantChooser all { numToGuess(int i, uint length)};
+   ConstantChooser all { numToGuess(i,length)};
    testcase.CHECK_EQUAL(play(all, all, 4, 100), 1);
         
 }
